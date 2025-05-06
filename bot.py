cb7e93297be5de40dcce9434e650a3a49306dfef
TvoeImya <you@example.com>
2025-05-06 08:25:22 +0500

    Обновила кнопки

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils import executor
 
 API_TOKEN = '7971577062:AAG28HRJHktjxk9bdvUz5C00U0zf0nLYBz0'
@@ -8,34 +8,32 @@ ADMIN_ID = 580812971  # Замените на свой Telegram ID
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# Сокращённый список направлений
 directions = [
-    "Соғлиқни сақлаш", "Таълим", "Транспорт", "Экология",
-    "Маҳалла", "Коррупция", "Энергетика", "Тоза ичимлик суви",
-    "Ёшлар сиёсати", "Қурилиш", "Ташқи иқтисод", "Бюджет",
-    "Маданият", "Туризм", "Спорт", "Қишлоқ хўжалиги", "Солиқ",
-    "Инвестиция", "Ижтимоий ҳимоя", "Жамоат транспорти",
-    "Чиқинди",  "Ижро ҳокимият",  "Ички ишлар"
-]
+    "Соғлиқни сақлаш", "Таълим", "Транспорт", "Экология", "Маҳалла", "Коррупция", "Энергетика",
+    "Тоза ичимлик суви", "Ёшлар сиёсати",  
+    "Қурилиш", "Ташқи иқтисод",  "Бюджет",  "Маданият", "Туризм", "Спорт",
+    "Қишлоқ хўжалиги", "Солиқ",  "Инвестиция", "Ижтимоий ҳимоя"
+   ,  "Жамоат транспорти", "Чиқинди", "Шаҳарсозлик", "Ижро ҳокимият",
+    "Суд", "Ички ишлар"
+ ]
 
 user_data = {}
 
 def get_direction_keyboard():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     buttons = [KeyboardButton(dir_name) for dir_name in directions]
     keyboard.add(*buttons)
     return keyboard
-
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    # Удалим старую клавиатуру и покажем новую
-    await message.answer("Клавиатура янгиланмоқда...", reply_markup=ReplyKeyboardRemove())
     await message.answer(
         "Ассалому алайкум!\n"
         "Сессияга тайёргарлик кўрилмоқда.\n"
         "Йўналишни танланг:",
         reply_markup=get_direction_keyboard()
     )
+
+    
 
 @dp.message_handler(lambda message: message.text in directions)
 async def handle_direction(message: types.Message):
